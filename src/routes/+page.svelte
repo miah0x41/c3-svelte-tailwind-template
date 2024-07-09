6efc6401f2f252baf492e012da5f2f94a85ca878
miah0x41 <99686292+miah0x41@users.noreply.github.com>
2024-07-09 17:37:45 +0100

    Initial commit.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col items-center justify-center h-screen">
+  <h1 class="text-4xl text-amber-800 font-bold">Curious Data Explorer</h1>
+  <p class="bg-amber-300/50 p-2 rounded-xl">Visit our blog: <a href="https://blog.curiodata.pro">https://blog.curiodata.pro</a></p>
+</div>
